e51d8d016f9936ec6d44a3995922f0ea0b86fe95
GATA5 <Tangjing.Gan@coop.ch>
2023-11-08 10:27:07 +0100

    Constructor

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,10 +2,16 @@ package org.example;
 
 public class Car {
 
-String marke = "BMW" ;
-int baujahr = 2019;
-int preis = 102000;
-int ps = 340;
-String modell = "X7";
-String farbe = "grau";
+    String marke = "BMW";
+    int baujahr = 2019;
+    int preis = 102000;
+    int ps = 340;
+    String modell = "X7";
+    String farbe = "grau";
+
+    Car(String marke, String modell, String farbe, int baujahr, int ps) {
+        this.marke = marke;
+        this.modell = modell;
+    }
+
 }
